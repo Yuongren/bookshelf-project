d5b439c4e96f13fb16c27c5412b3eee9d458c709
gitYoungren <youngrengitonga@gmail.com>
2023-01-26 23:37:06 +0300

    files added in bookcard

---
+++ src/BookCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Card({ book, addToShelf }) {
+    return (
+        <>
+            {book.map((item, index) => {
+                let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                // if(thumbnail!== undefined && amount !== undefined)
+                // {
+                return (
+
+                    <div className="card"  key={index}>
+                        <img className="card-img-top" src={thumbnail} alt="" />
+                        <div className="card-body">
+                            <h6 className="card-title">{item.volumeInfo.title}</h6>
+                            <p className="card-text">By:{item.volumeInfo.authors}</p>
+                            <p className="card-text">{amount}</p>
+                            <button className="btn btn-primary"
+                                onClick={() => addToShelf(item)}>ADD TO BOOKSHELF</button>
+                        </div>
+                    </div>
+                );
+
+
+
+            })}
+        </>
+    );
+}
+export default Card;
