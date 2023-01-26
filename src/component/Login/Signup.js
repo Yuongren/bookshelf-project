ac95ace7c5885500dbdc264bb019da6b3f7b310b
gitYoungren <youngrengitonga@gmail.com>
2023-01-26 23:34:37 +0300

    changes done in signup file

---
+++ src/component/Login/Signup.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+function Signup (props) {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+    return (
+        <div className="auth-form-container">
+            <h2>Signup</h2>
+        <form className="signup-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="fullname" id="name" name="name" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@example.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="************" id="password" name="password" />
+            <button type="submit">Login</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch("login")}>Already have an account? login.</button>
+    </div>
+    )
+}
+export default Signup;
