440aba207a143d15ff364acb595eda3ad5cf448f
gitYoungren <youngrengitonga@gmail.com>
2023-01-27 00:47:39 +0300

    changes made in home file

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Home() {
-  return (<div>{/*{code here}*/}
+  return (<div className="home-style">
   <h1 className='header-one'>BOOKSHELF</h1>
-      <p> Bokshelf is an application that allows users to browse and discover new books, as well as keep track of their personal reading collection.</p>
-      <p>The main feature of the app is the ability to add books to a custom "bookshelf". Users can search for books by title, author, or genre, and then add them to their bookshelf by clicking a button. The bookshelf is organized in a way that allowing users to easily find and access their favorite books with ease.</p>
-      <p>The app has a simple and easy-to-use interface, with a clean and modern design.</p>
-      <p>Overall, Bookshelf is a great tool for book lovers of all ages. It offers a convenient way to discover new books.</p>
-      <p>Hope you enjoy the experience. Cheers!</p>
+      <p className="para"> Bokshelf is an application that allows users to browse and discover new books, as well as keep track of their personal reading collection.</p>
+      <p className="para">The main feature of the app is the ability to add books to a custom "bookshelf". Users can search for books by title, author, or genre, and then add them to their bookshelf by clicking a button. The bookshelf is organized in a way that allowing users to easily find and access their favorite books with ease.</p>
+      <p className="para">The app has a simple and easy-to-use interface, with a clean and modern design.</p>
+      <p className="para">Overall, Bookshelf is a great tool for book lovers of all ages. It offers a convenient way to discover new books.</p>
+      <p className="para">Hope you enjoy the experience. Cheers!</p>
   </div>);
 }
 
