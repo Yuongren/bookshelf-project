3516a3a1f50916036653fca4161acdb1d773c9fe
gitYoungren <youngrengitonga@gmail.com>
2023-01-26 23:39:25 +0300

    content added in home file

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (<div>{/*{code here}*/}
+  <h1 className='header-one'>BOOKSHELF</h1>
+      <p> Bokshelf is an application that allows users to browse and discover new books, as well as keep track of their personal reading collection.</p>
+      <p>The main feature of the app is the ability to add books to a custom "bookshelf". Users can search for books by title, author, or genre, and then add them to their bookshelf by clicking a button. The bookshelf is organized in a way that allowing users to easily find and access their favorite books with ease.</p>
+      <p>The app has a simple and easy-to-use interface, with a clean and modern design.</p>
+      <p>Overall, Bookshelf is a great tool for book lovers of all ages. It offers a convenient way to discover new books.</p>
+      <p>Hope you enjoy the experience. Cheers!</p>
+  </div>);
+}
+
+export default Home;
