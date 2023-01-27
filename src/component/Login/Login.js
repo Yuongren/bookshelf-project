0e5188b4dbf3c83ab4d1e23c480e316005d5c716
gitYoungren <youngrengitonga@gmail.com>
2023-01-27 08:50:13 +0300

    license file added

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -15,7 +15,7 @@ function Login(props) {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@example.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="************" id="password" name="password" />
-                <button type="submit">Login</button>
+                <button className="login-btn" type="submit">Login</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch("sign up")}>Don't have an account? Sign up.</button>
         </div>
